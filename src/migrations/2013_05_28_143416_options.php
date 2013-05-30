fe2908807daa2c55b333a84e4cdcc4847604f3c5
Israel Ortuño <israel@expander.es>
2013-05-31 00:45:48 +0200

    More changes in migrations

--- src/migrations/2013_05_28_143416_options.php
+++ src/migrations/2013_05_28_143416_options.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOptionsTable extends Migration {
+class Options extends Migration {
 
 	/**
 	 * Run the migrations.
